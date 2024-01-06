b5376874772323bd37f1dfdf91e12eb8fa4e880a
MonoDG <monodg@proton.me>
2024-01-05 20:33:21 -0500

    Update adj list, each element is a couple (i,j)

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ let kinghtMovements = [
 ];
 
 function movePiece(i, j) {
-    // let board = Array.from(Array(8), () => new Array(8).fill(0));
     kinghtMovements.forEach(([offsetX, offsetY]) => {
         if (
             i + offsetX >= 0 &&
@@ -23,32 +22,28 @@ function movePiece(i, j) {
             i + offsetX < 8 &&
             j + offsetY < 8
         ) {
-            // board[i + offsetX][j + offsetY] = "X";
-            adjList[i * 8 + j].push([i + offsetX, j + offsetY]);
+            adjList[i][j].push([i + offsetX, j + offsetY]);
         }
     });
-    // let output = "";
-    // for (let i = 0; i < board.length; i++) {
-    //     output += "|";
-    //     for (let j = 0; j < board.length; j++) {
-    //         output += `${board[i][j]}|`;
-    //     }
-    //     output += "\n";
-    // }
-    // console.log(output);
 }
 
 function createAdjacencyList() {
-    return Array.from(Array(64), () => new Array());
+    return Array.from(Array(8), () => Array.from(Array(8), () => new Array()));
 }
 
 let adjList = createAdjacencyList();
 for (let i = 0; i < 8; i++) {
     for (let j = 0; j < 8; j++) {
-        console.log(`KNIGHT IS AT [${i}, ${j}]`);
         movePiece(i, j, adjList);
-        console.log("");
     }
 }
-
-console.log(adjList);
+let output = "";
+adjList.forEach((row) => {
+    row.forEach((column) => {
+        output += `Neighbors for vertex: `;
+        column.forEach((neighbor) => (output += `(${neighbor}),`));
+        output += "\n";
+    });
+    output += "\n";
+});
+console.log(output);
